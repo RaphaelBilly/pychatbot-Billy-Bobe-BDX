29a35eef740c3a899fb18ff661d36654884489e6
RaphaelBilly <raphael.billy@efrei.net>
2023-11-17 10:16:07 +0100

    Added chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,21 @@
+import os
+
+#Récupère les noms des fichiers contenants les discours
+def list_of_files(directory, extension):
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
+
+directory = "speeches"
+files_names = list_of_files(directory, "txt")
+print(files_names)
+
+
+
+# def pretraitement(fichier):
+#     for i in
+
+
+
