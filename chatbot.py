db0a8db6da5ddbbef58ca60252389814ff40fd41
RaphaelBilly <raphael.billy@efrei.net>
2023-11-21 09:53:59 +0100

    Added function Extraire_nom

--- chatbot.py
+++ chatbot.py
@@ -8,14 +8,39 @@ def list_of_files(directory, extension):
             files_names.append(filename)
     return files_names
 
-directory = "speeches"
+directory = "./speeches"
 files_names = list_of_files(directory, "txt")
 print(files_names)
 
-print("Raphael est trop chaud en python")
+def extraire_nom(files_names):
+    liste_nom_president = []
+    for i in range(len(files_names)):
+        digit = False
+        files_names[i]= files_names[i][11:]
+        for j in files_names[i]:
+            if j.isdigit():
+                digit = True
+        if digit == True:
+            files_names[i] = files_names[i][:-5]
+        else :
+            files_names[i] = files_names[i][:-4]
+    for i in range (len(files_names)):
+        if files_names[i] not in liste_nom_president:
+            liste_nom_president.append(files_names[i])
+    print(liste_nom_president)
+    return liste_nom_president
 
-# def pretraitement(fichier):
-#     for i in
+
+
+print(extraire_nom(files_names))
+
+
+
+
+
+
+
+
 
 
 
