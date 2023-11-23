4158ca5036f51d9e147edbb7904e3ef19735c7f0
Louis Bobe <louisbobe24@gmail.com>
2023-11-23 19:32:30 +0100

    Add TF-IDF.py Code tf function

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,11 +1,22 @@
-def tf(chaine_de_caractere):
+from math import log10
+import os
+def TF_calculator(chaine_de_caractere):
     tableau = chaine_de_caractere.split(" ")
     dic = {}
-    for element in tableau :
-        if element not in dic.keys() :
+    for element in tableau:
+        if element not in dic.keys():
             dic[element] = 1
-        else :
+        else:
             dic[element] += 1
     return dic
 
-print(tf("il est au minimum nécessaire d’avoir  au qui le le au qui une fonction qui calcule le nombre d’occurrence de chaque mot dans un texte"))
+
+def IDF_calculator(directory):
+    dic = {}
+    for files in directory :
+
+
+if __name__ == "__main__":
+    print(TF_calculator(
+        "il est au minimum nécessaire d’avoir  au qui le le au qui une fonction qui calcule le nombre d’occurrence de chaque mot dans un texte"))
+    directory = "./speeches"
