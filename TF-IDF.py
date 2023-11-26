c89076108ae29f2cb7598e490aa29e4a16525dcd
Louis Bobe <louisbobe24@gmail.com>
2023-11-26 18:55:20 +0100

    Add TF-IDF.py Modified TF_calculator() (taking a file instead of a string in argument +added TF_IDF_calculator fonction

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,15 +1,7 @@
 from math import log10
 import os
 from chatbot import list_of_files
-def TF_calculator(chaine_de_caractere):
-    tableau = chaine_de_caractere.split(" ")
-    dic = {}
-    for element in tableau:
-        if element not in dic.keys():
-            dic[element] = 1
-        else:
-            dic[element] += 1
-    return dic
+
 
 def list_of_words(file):
     liste = []
@@ -20,6 +12,23 @@ def list_of_words(file):
         for word in line :
             if word not in liste :
                 liste.append(word)
+
+def TF_calculator(file):
+    dic = {}
+    fichier = open(".speeches/" + file, "r")
+    lines = fichier.readlines()
+    for line in lines:
+        line = line.split(" ")
+        for word in line
+            if word not in dic.keys():
+                dic[word] = 1
+            else:
+                dic[word] += 1
+    liste = list_of_words(file)
+    for word in liste :
+        dic[word] = dic[word] / len(liste)
+    return dic
+
 def IDF_calculator(directory):
     dic = {}
     files_names = list_of_files((directory, ".txt"))
@@ -37,6 +46,22 @@ def IDF_calculator(directory):
         dic [key] = log10(ratio)
     return dic
 
+
+def TF_IDF_calculator(directory):
+    files_names = list_of_files(directory, ".txt")
+    matrice = []
+    IDF_scores = IDF_calculator(directory)
+    list_of_all_words = IDF_scores.keys()
+    for word in list_of_all_words:
+        word_TFIDF_scores = []
+        for file in files_names :
+            TF_scores = TF_calculator(file):
+            word_TF_score = TF_scores[word]
+            word_IDF_score = IDF_scores[word]
+            word_TFIDF_score = word_TF_score * word_IDF_score
+            word_TFIDF_scores.append(word_TFIDF_score)
+        matrice.append(word_TFIDF_scores)
+    return matrice, list_of_all_words
 
 if __name__ == "__main__":
     print(TF_calculator(
