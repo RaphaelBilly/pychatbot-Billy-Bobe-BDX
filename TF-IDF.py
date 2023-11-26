9800c2571a5890e22e6ac6ec1c7f97fe57953764
Louis Bobe <louisbobe24@gmail.com>
2023-11-26 19:17:46 +0100

    Add TF-IDF.py Modified TF_calculator() (taking a file instead of a string in argument +added TF_IDF_calculator fonction

--- TF-IDF.py
+++ TF-IDF.py
@@ -7,11 +7,12 @@ def list_of_words(file):
     liste = []
     fichier = open(".speeches/" + file, "r")
     lines = fichier.readlines()
-    for line in lines :
+    for line in lines:
         line = line.split(" ")
-        for word in line :
-            if word not in liste :
+        for word in line:
+            if word not in liste:
                 liste.append(word)
+
 
 def TF_calculator(file):
     dic = {}
@@ -25,25 +26,26 @@ def TF_calculator(file):
             else:
                 dic[word] += 1
     liste = list_of_words(file)
-    for word in liste :
+    for word in liste:
         dic[word] = dic[word] / len(liste)
     return dic
+
 
 def IDF_calculator(directory):
     dic = {}
     files_names = list_of_files((directory, ".txt"))
     number_of_files = len(files_names)
-    for file in files_names :
+    for file in files_names:
         list_of_words = list_of_words(file)
-        for word in list_of_words :
-            if word not in dic.keys() :
+        for word in list_of_words:
+            if word not in dic.keys():
                 dic[word] = 1
-            else :
+            else:
                 dic[word] += 1
-    for key in dic.keys() :
+    for key in dic.keys():
         value = dic[key]
-        ratio = number_of_files/value
-        dic [key] = log10(ratio)
+        ratio = number_of_files / value
+        dic[key] = log10(ratio)
     return dic
 
 
@@ -54,7 +56,7 @@ def TF_IDF_calculator(directory):
     list_of_all_words = IDF_scores.keys()
     for word in list_of_all_words:
         word_TFIDF_scores = []
-        for file in files_names :
+        for file in files_names:
             TF_scores = TF_calculator(file):
             word_TF_score = TF_scores[word]
             word_IDF_score = IDF_scores[word]
@@ -62,6 +64,7 @@ def TF_IDF_calculator(directory):
             word_TFIDF_scores.append(word_TFIDF_score)
         matrice.append(word_TFIDF_scores)
     return matrice, list_of_all_words
+
 
 if __name__ == "__main__":
     print(TF_calculator(
