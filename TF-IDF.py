a059ec4338b092415481e46d5d916983f13c6567
Louis Bobe <louisbobe24@gmail.com>
2023-12-04 01:14:18 +0100

    changed TF-IDF.py added mots_pas_importants and mots_importants functions + add if name == main

--- TF-IDF.py
+++ TF-IDF.py
@@ -8,6 +8,7 @@ def get_list_of_words(file: str) -> list:
     file = open("./cleaned/" + file, "r", encoding="utf-8")
     return list(set(file.read().split(" ")[:-1]))
 
+
 def get_term_frequency(file: str) -> dict:
     list_of_words = get_list_of_words(file)
     term_frequency = {}
@@ -18,8 +19,6 @@ def get_term_frequency(file: str) -> dict:
         else:
             term_frequency[word] += 1
     return term_frequency
-
-
 
 
 def get_inverse_document_frequency(directory: str) -> dict:
@@ -50,7 +49,7 @@ def get_tf_id_matrix(directory: str) -> list:
     inverse_document_frequency = get_inverse_document_frequency(directory)
     list_of_words = inverse_document_frequency.keys()
     word_list = []
-    for word in list_of_words :
+    for word in list_of_words:
         word_list.append(word)
         word_idf_score = inverse_document_frequency[word]
         word_tf_idf_scores = []
@@ -58,9 +57,9 @@ def get_tf_id_matrix(directory: str) -> list:
             tf_scores = get_term_frequency(file_name)
             if word in get_list_of_words(file_name):
                 word_tf_score = tf_scores[word]
-            else :
+            else:
                 word_tf_score = 0
-            word_tf_idf_score = word_tf_score*word_idf_score
+            word_tf_idf_score = word_tf_score * word_idf_score
             word_tf_idf_scores.append(word_tf_idf_score)
         tf_id_matrix.append(word_tf_idf_scores)
     return tf_id_matrix, word_list
@@ -68,12 +67,13 @@ def get_tf_id_matrix(directory: str) -> list:
 
 def mots_pas_importants(matrice, liste_de_mots):
     nombre_textes = len(matrice[0])
-    score_nul = [0.0]*nombre_textes
+    score_nul = [0.0] * nombre_textes
     mots_pas_importants = []
     for i in range(len(matrice)):
-        if matrice[i] == score_nul :
+        if matrice[i] == score_nul:
             mots_pas_importants.append(liste_de_mots[i])
     return mots_pas_importants
+
 
 def mots_importants(matrice, liste_de_mots):
     mots_importants = [liste_de_mots[0]]
@@ -81,17 +81,16 @@ def mots_importants(matrice, liste_de_mots):
     for i in range(len(matrice[0])):
         score += matrice[0][i]
     max = score
-    for i in range(1,len(matrice)):
+    for i in range(1, len(matrice)):
         score = 0
         for j in range(len(matrice[i])):
             score += matrice[i][j]
-        if score >= max :
-            if score > max :
+        if score >= max:
+            if score > max:
                 max = score
                 mots_importants.clear()
             mots_importants.append(liste_de_mots[i])
     return mots_importants
-
 
 
 if __name__ == "__main__":
@@ -99,4 +98,3 @@ if __name__ == "__main__":
     print(get_inverse_document_frequency("./cleaned"))
     matrice, word_list = get_tf_id_matrix("./cleaned")
     print(mots_importants(matrice, word_list))
-
